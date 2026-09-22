3b519b2ba00e32e55b38c04a140316c87c673328
Gabriel de Oliveira <125375106+gabrieldeoliveira1@users.noreply.github.com>
2026-09-22 09:30:05 -0300

    Refactor Tutor class constructor and methods

--- Tutor.java
+++ Tutor.java
@@ -3,68 +3,73 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class Tutor {
-    private static int proximoCod = 1;
-    private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+   private static final DateTimeFormatter FORMATADOR_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    private int cod;
-    private String nomeTutor;
-    private String endereco;
-    private LocalDate dataNasc;
-    private ArrayList<Pet> pets;
+   private int cod;
+   private String nomeTutor;
+   private String endereco;
+   private LocalDate dataNasc;
+   private ArrayList<Pet> pets;
 
-    public Tutor(String nomeTutor, String endereco, LocalDate dataNasc) {
-        this.cod = proximoCod;
-        proximoCod++;
-        this.nomeTutor = nomeTutor;
-        this.endereco = endereco;
-        this.dataNasc = dataNasc;
-        this.pets = new ArrayList<>();
-    }
+   public Tutor(int cod, String nomeTutor, String endereco, LocalDate dataNasc) {
+      this.cod = cod;
+      this.nomeTutor = nomeTutor;
+      this.endereco = endereco;
+      this.dataNasc = dataNasc;
+      this.pets = new ArrayList<>();
+   }
 
-    public int getCod() {
-        return cod;
-    }
+   public int getCod() {
+      return cod;
+   }
 
-    public String getNomeTutor() {
-        return nomeTutor;
-    }
+   public String getNomeTutor() {
+      return nomeTutor;
+   }
 
-    public String getEndereco() {
-        return endereco;
-    }
+   public String getEndereco() {
+      return endereco;
+   }
 
-    public LocalDate getDataNasc() {
-        return dataNasc;
-    }
+   public LocalDate getDataNasc() {
+      return dataNasc;
+   }
 
-    public ArrayList<Pet> getPets() {
-        return pets;
-    }
+   public ArrayList<Pet> getPets() {
+      return pets;
+   }
+   
+   //faz validacao para garantir que nao terao mais de um pet com o mesmo nome para o mesmo tutor 
+   public boolean incluiPet(String nomePet, String tipoPet) {
+      for (Pet p : pets) {
+         if (p.getNomePet().equalsIgnoreCase(nomePet)) {
+            return false;
+         }
+      }
+      pets.add(new Pet(nomePet, tipoPet));
+      return true;
+   }
 
-    public void adicionarPet(Pet pet) {
-        pets.add(pet);
-    }
+   public void imprimirInformacoes() {
+      System.out.println("Tutor #" + cod);
+      System.out.println("Nome: " + nomeTutor);
+      System.out.println("Endereco: " + endereco);
+      System.out.println("Data de nascimento: " + dataNasc.format(FORMATADOR_DATA));
+   
+      if (pets.isEmpty()) {
+         System.out.println("Pets: sem pets cadastrados");
+         return;
+      }
+   
+      System.out.println("Pets:");
+      for (int i = 0; i < pets.size(); i++) {
+         pets.get(i).imprimirInformacoes(i + 1);
+      }
+   }
 
-    public void imprimirInformacoes() {
-        System.out.println("Tutor #" + cod);
-        System.out.println("Nome: " + nomeTutor);
-        System.out.println("Endereco: " + endereco);
-        System.out.println("Data de nascimento: " + dataNasc.format(FORMATADOR_DATA));
-
-        if (pets.isEmpty()) {
-            System.out.println("Pets: sem pets cadastrados");
-            return;
-        }
-
-        System.out.println("Pets:");
-        for (int i = 0; i < pets.size(); i++) {
-            pets.get(i).imprimirInformacoes(i + 1);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Tutor {cod=" + cod + ", nome='" + nomeTutor + "', endereco='" + endereco
-                + "', dataNasc=" + dataNasc + ", pets=" + pets + "}";
-    }
+   @Override
+   public String toString() {
+      return "Tutor {cod=" + cod + ", nome='" + nomeTutor + "', endereco='" + endereco
+             + "', dataNasc=" + dataNasc + ", pets=" + pets + "}";
+   }
 }
